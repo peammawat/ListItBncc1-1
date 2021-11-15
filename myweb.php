bb7e93067e7286849cadec96f72df1dbd28b5750
peammawat <peammawat.thongpat@gmail.com>
2021-11-15 16:34:46 +0700

    start new project

---
+++ myweb.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BNCC IT 1/1 WEBSITE</title>
+    <link rel="icon" type="image/x-icon" href="logo.png">
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+th, td {
+  border-radius: 10px;
+  border-style:solid;
+  border-color: MediumSeaGreen;
+  font-size: 30px;
+  text-align: center;
+}
+
+.nametd {
+    text-align: left;
+}
+</style>
+</head>
+<body>
+    <img src="logo.png" alt="mainlogo" weight="350" height="350" >
+
+    <h1>สวัสดีวัยรุ่น BNCC IT 1 / 1</h1>
+
+   
+
+    
+        <h1>BNCC IT</h1>
+<table>
+    <thead>
+    <tr>
+        <th>เลขที่</th>
+        <th>รหัสประจำตัว</th>
+        <th>ชื่อ</th>
+        <th>นามสกุล</th>
+    </tr>
+</thead>
+    <tr>
+        <td>1</td>
+        <td>64209010001</td>
+        <td class="nametd">นาย สุเมธ</td>
+        <td class="nametd">อัครวงษ์</td>
+    </tr>
+
+    <tr>
+        <td>2</td>
+        <td>64209010002</td>
+        <td class="nametd">นางสาว สิรินยา</td>
+        <td class="nametd">มีคะนุช</td>
+    </tr>
+
+    <tr>
+        <td>3</td>
+        <td>64209010003</td>
+        <td class="nametd">นางสาว อนงค์ภัสสร</td>
+        <td class="nametd">มูสิกะปาละ</td>
+    </tr>
+
+    <tr>
+        <td>4</td>
+        <td>64209010004</td>
+        <td class="nametd">นาย ภัทร</td>
+        <td class="nametd">การะเกตุ</td>
+    </tr>
+
+    <tr>
+        <td>5</td>
+        <td>64209010005</td>
+        <td class="nametd">นาย สถาพร</td>
+        <td class="nametd">สุขสวัสดิ์</td>
+    </tr>
+
+    <tr>
+        <td>6</td>
+        <td>64209010006</td>
+        <td class="nametd">นาย ชยกร</td>
+        <td class="nametd">ลู่วิจิตรบรรจง</td>
+    </tr>
+
+    <tr>
+        <td>7</td>
+        <td>64209010007</td>
+        <td class="nametd">นางสาว จิณห์วรา</td>
+        <td class="nametd">เจริญสระน้อน</td>
+    </tr>
+
+    <tr>
+        <td>8</td>
+        <td>64209010007</td>
+        <td class="nametd">นางสาว ปัณฑิตา</td>
+        <td class="nametd">เทียนไสย</td>
+    </tr>
+
+    <tr>
+        <td>9</td>
+        <td>64209010008</td>
+        <td class="nametd">นางสาว ณัฐภัทร</td>
+        <td class="nametd">ณฤกษ์</td>
+    </tr>
+
+    <tr>
+        <td>10</td>
+        <td>64209010010</td>
+        <td class="nametd">นาย สุทธิพร</td>
+        <td class="nametd">พุ่มทอง</td>
+    </tr>
+
+    <tr>
+        <td>11</td>
+        <td>64209010011</td>
+        <td class="nametd">นาย ภานุพงศ์</td>
+        <td class="nametd">ไกรงู</td>
+    </tr>
+
+    <tr>
+        <td>12</td>
+        <td>64209010012</td>
+        <td class="nametd">นาย นันทวัช</td>
+        <td class="nametd">ไกรสุข</td>
+    </tr>
+
+    <tr>
+        <td>13</td>
+        <td>64209010013</td>
+        <td class="nametd">นางสาว ตวงพร</td>
+        <td class="nametd">สุขสิริรุ่ง</td>
+    </tr>
+
+    <tr>
+        <td>14</td>
+        <td>64209010014</td>
+        <td class="nametd">นาย อาริยะ</td>
+        <td class="nametd">บุญประเสริฐ</td>
+    </tr>
+
+    <tr>
+        <td>15</td>
+        <td>64209010015</td>
+        <td class="nametd">นาย อรรถพร</td>
+        <td class="nametd">เพชรรัตน์</td>
+    </tr>
+
+
+
+    </table>
+
+</body>
+</html>
